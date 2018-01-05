92abff9dbf2368a465985518ba933cbabe4ca375
天空小小 <751634819@qq.com>
2018-01-05 12:21:37 +0800

    Update Consumer.java

--- dubbo-demo/dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/Consumer.java
+++ dubbo-demo/dubbo-demo-consumer/src/main/java/com/alibaba/dubbo/demo/consumer/Consumer.java
@@ -8,7 +8,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Created by ken.lj on 2017/7/31.
  */
 public class Consumer {
-
+edfs
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-consumer.xml"});
         context.start();
